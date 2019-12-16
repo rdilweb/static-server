0350ac3382b38902ac30e8b4529f1134b283296c
Reece Dunham <me@rdil.rocks>
2019-12-16 20:29:05 +0000

    tweaks

---
+++ source/index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+import { createServer } from "http"
+import { parse } from "url"
+import { join, extname } from "path"
+import { stat as _stat, createReadStream } from "fs"
+import { lookup } from "mime"
+import mnm from "minimist"
+
+var argv = mnm(process.argv.slice(2)),
+    port = argv.p || 7788,
+    root = argv.r || process.cwd(),
+    fallbackPath = argv.f
+
+if (argv.h) {
+    console.log("\nNODE STATIC SERVER")
+    console.log(
+        "   Simple static web server.\
+    \n   Author: Van-Duyet Le (me@duyetdev.com). \
+    \n   Website: http://duyetdev.com"
+    )
+    console.log(
+        "\nUsage:\
+    \n    static-html-server -p [port] -r [root folder] -f [fallback path if not found]"
+    )
+    console.log(
+        "\nArguments (all are optional):\
+    \n    - p: [Number] port number, default to 8000\
+    \n    - r: [String] root folder, default to working directory\
+    \n    - f: [String] fallback path when page not found, default to not falling back and send 404\n\n"
+    )
+    console.log(
+        "For example\
+    \n    $ static-html-server -p 8899 -r ./ -f index.html\
+    \n    Server running at http://localhost:8899/ [root: ./, fallback: index.html]\n"
+    )
+    process.exit(1)
+}
+
+if (fallbackPath) fallbackPath = join(root, fallbackPath)
+
+createServer(function requestHandler(req, res) {
+    var uriPath = parse(req.url).pathname,
+        filePath = join(root, unescape(uriPath))
+
+    console.log("Serving " + uriPath)
+    handle(filePath)
+
+    function handle(filePath, fallingback) {
+        _stat(filePath, function(err, stat) {
+            if (err) {
+                if (err.code == "ENOENT") {
+                    if (!fallingback && fallbackPath)
+                        return handle(fallbackPath, true)
+                    res.statusCode = 404
+                } else res.statusCode = 500
+                res.end()
+                console.error(err)
+            } else if (stat.isDirectory()) {
+                handle(join(filePath, "index.html"))
+            } else {
+                var contentType = lookup(extname(filePath))
+                res.writeHead(200, { "Content-Type": contentType })
+                createReadStream(filePath).pipe(res)
+            }
+        })
+    }
+}).listen(port)
+
+console.log(
+    "Server running at http://localhost:" +
+        port +
+        "/" +
+        " [root: " +
+        root +
+        ", fallback: " +
+        fallbackPath +
+        "]"
+)
